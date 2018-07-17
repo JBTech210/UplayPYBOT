a16ed282407f5d251cf1bc6a238a95ec2c5fd1e2
JBTech210 <40965733+JBTech210@users.noreply.github.com>
2018-07-17 20:57:14 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import random
+
+Client = discord.Client()
+bot = commands.Bot(command_prefix="<")
+lines = open(r'uplay.txt').read().splitlines()
+
+@bot.event
+async def on_ready():
+    print('The bot is online!')
+    await bot.change_presence(game=discord.Game(name='Type <uplay'))
+   
+    
+@bot.command(pass_context=True)
+async def uplay(ctx):
+    userName = ctx.message.author.name
+    userID = ctx.message.author.id
+
+    if ctx.message.server:
+        myline = random.choice(lines)
+        split = myline.partition(":")
+        
+        embed=discord.Embed(title="Minecraft Account", color=0xf45eff)
+        embed.set_thumbnail(url="https://pre00.deviantart.net/3d13/th/pre/i/2016/343/0/7/free_minecraft_logo_template_by_curtzeninja-dar2dou.png")
+        embed.add_field(name="Email:", value=split[0], inline=False)
+        embed.add_field(name="Password:", value=split[2], inline=False)
+        await bot.send_message(ctx.message.author, embed=embed)
+
+        print("{} Typed <uplay".format(userName))
+
+        client = discord.Client()
+
+    
+bot.run("NDY4ODQ4MTIxODIxMTM0ODUw.Di_IXA.CFcCZi0QRCVj_YXICGAzLZ8tI48")
